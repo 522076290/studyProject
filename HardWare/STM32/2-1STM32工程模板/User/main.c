840945939b48490184ab1135d41bca866a945052
xcw <522076290@qq.com>
2024-06-16 17:11:21 +0800

    STM学习2-1

---
+++ HardWare/STM32/2-1STM32工程模板/User/main.c
@@ -0,0 +1,23 @@
+#include "stm32f10x.h"                  // Device header
+
+int main(void)
+{
+	// 使用控制寄存器的方式
+	// RCC -> APB2ENR = 0x00000010;
+	// GPIOC -> CRH = 0x00300000;
+	// GPIOC -> ODR = 0x00002000;
+	
+	// 使用库函数方式
+	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC,ENABLE);
+	GPIO_InitTypeDef GPIO_InitStructure;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_Out_PP;
+	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13;
+	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+	GPIO_Init(GPIOC,&GPIO_InitStructure);
+	GPIO_SetBits(GPIOC,GPIO_Pin_13);
+	//GPIO_ResetBits(GPIOC,GPIO_Pin_13);
+	while(1)
+	{
+		
+	}
+}
